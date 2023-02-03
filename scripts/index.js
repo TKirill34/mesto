e6021754694b82b3353a4519322ac90429b10568
Kirill Toropov <kv34practicum@yandex.ru>
2023-02-03 21:36:42 +0700

    feat: написать скрипт для модального окна

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__close");
+const formElement = popup.querySelector(".popup__form");
+const nameInput = popup.querySelector(".popup__profile-name");
+const jobInput = popup.querySelector(".popup__profile-profession");
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
+
+const toggleOpenPopup = () => {
+	popup.classList.toggle('popup_opened');
+}
+
+const handleEditButtonClick = () => {
+	nameInput.value = profileName.textContent;
+	jobInput.value = profileProfession.textContent;
+	toggleOpenPopup();
+}
+
+const handleCloseButtonClick = () => {
+	toggleOpenPopup();
+}
+
+function handleFormSubmit (evt) {
+	evt.preventDefault();
+	profileName.textContent = nameInput.value;
+	profileProfession.textContent = jobInput.value;
+	toggleOpenPopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
